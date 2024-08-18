983430f82d680ca31231c5f979be475368f4c4b9
Difz25x <difz25x@gmail.com>
2024-08-18 16:52:44 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,7 +4,7 @@ local playerGui = player:WaitForChild("PlayerGui")
 
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
-local SpeedSlider = Instance.new("Slider")
+local SpeedSlider = Instance.new("UISlider") -- Assuming UISlider is the correct type for a slider
 local SpeedLabel = Instance.new("TextLabel")
 local ModifySpeedButton = Instance.new("TextButton")
 
@@ -26,18 +26,24 @@ SpeedSlider.Size = UDim2.new(1, -20, 0, 30)
 SpeedSlider.Position = UDim2.new(0, 10, 0, 50)
 SpeedSlider.MinValue = 16
 SpeedSlider.MaxValue = 100
+SpeedSlider.Value = 16 -- Initial value
 
 ModifySpeedButton.Parent = Frame
 ModifySpeedButton.Size = UDim2.new(1, 0, 0, 50)
 ModifySpeedButton.Position = UDim2.new(0, 0, 0, 80)
 ModifySpeedButton.Text = "Modify Speed"
 
-local speedModified = true
-
-ModifySpeedButton.MouseButton1Click:Connect(function()
-    if speedModified then
-        local newSpeed = SpeedSlider:Callback(function(Value)
+local function modifySpeed()
+    local newSpeed = SpeedSlider.Value
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
         player.Character.Humanoid.WalkSpeed = newSpeed
         SpeedLabel.Text = "Walk Speed: " .. newSpeed
     end
+end
+
+ModifySpeedButton.MouseButton1Click:Connect(modifySpeed)
+
+-- Optionally, connect the slider's value changed event to update the speed dynamically
+SpeedSlider.ValueChanged:Connect(function()
+    modifySpeed()
 end)
